2fee98dd2feb718b6866775420464663c4ae878c
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-12-26 09:51:49 +0100

    .

--- communicate.py
+++ communicate.py
@@ -9,23 +9,25 @@ import smtplib, ssl
 
 from datetime import datetime
 
-stream = os.popen("curl ipecho.net/plain")
-message = stream.read()
-
 user = input("Username in gmail (without <@gmail.com>? ")
 password = input("Password in gmail? ")
 
 sender_email = user + "@gmail.com"
 receiver_email = user + "@gmail.com"
 
+old = ""
 while True:
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        #sys.stdout.write(f"{dt_string}: Sending \"{message}\" ... "); sys.stdout.flush()
-        sys.stdout.write("{}: : Sending \"{}\" ... ".format(dt_string,message))
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-        sys.stdout.write("done\n");
-    time.sleep(3600)
+    stream = os.popen("curl ipecho.net/plain")
+    message = stream.read()
+    if old != message:
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            #sys.stdout.write(f"{dt_string}: Sending \"{message}\" ... "); sys.stdout.flush()
+            sys.stdout.write("{}: : Sending \"{}\" ... ".format(dt_string,message))
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+            sys.stdout.write("done\n");
+    old = message
+    time.sleep(1)
